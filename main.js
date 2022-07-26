ed9248b89d20ab10167abf2e9deb072005c06bc5
20181020076 <diegoarojas@hotmail.es>
2022-07-26 16:21:23 -0500

    version final, js implementado para validar el correo

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-// Please provide a vaild email
 const form = document.querySelector('.container__content__form');
 const inputEmail = document.querySelector('.container__content__form__email');
 const error = document.querySelector('.container__content__form__email--error');
 
 form.addEventListener('submit',(event)=>{
-    event.preventDefault();
-    console.log("se ha presionado el boton")
+
+    const regExp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;  //validacion de email con expresiones regulares de la pagina www.regex.com
+    if(regExp.test(inputEmail.value)==true){
+        inputEmail.style.border='1px solid hsl(0, 36%, 70%)';
+        inputEmail.style.backgroundImage= 'url("")';
+        error.innerHTML = "";
+    }else{
+        event.preventDefault();
+        inputEmail.style.border='1px solid red';
+        inputEmail.style.backgroundImage= 'url("./images/icon-error.svg")';
+        error.innerHTML = "Please provide a vaild email"
+    }
 });
